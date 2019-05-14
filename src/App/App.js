319eb008aa081969377b5d7729fedc9bf8f19353
ducaale <sharaf.13@hotmail.com>
2019-05-14 14:56:40 +0300

    use islamic-inheritance-calculator lib from github
    also disabled pnp

--- src/App/App.js
+++ src/App/App.js
@@ -2,7 +2,7 @@
 import React, { useState, useReducer, useMemo } from 'react';
 import Solution from '../Solution/Solution'
 import Input from '../Input/Input'
-import { defaultHeirs, calculate } from '@islamic-inheritance-calculator/core'
+import { defaultHeirs, calculate } from 'islamic-inheritance-calculator'
 
 import Fab from '@material/react-fab'
 import Button from '@material/react-button'
